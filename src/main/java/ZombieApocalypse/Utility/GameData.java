66d08da85b89c1080773a36567fc4e5730ed1758
Matteo Canino <cnnmtt02p18c352y@studenti.unical.it>
2023-05-31 12:53:31 +0200

    Settings pt.9

--- src/main/java/ZombieApocalypse/Utility/GameData.java
+++ src/main/java/ZombieApocalypse/Utility/GameData.java
@@ -5,7 +5,7 @@ public class GameData {
     public static String nick;  //Variabile che contiene il nickname del player
     public static String pass;  //Variabile che contiene la password del player
     public static String version = "V1.0.0";    //Variabile per rappresentare la versione
-    public static boolean music = true;     //Variabile per la musica muta
+    public static boolean music = false;     //Variabile per la musica muta
     public static boolean sound = true;     //variabile per i suoni muti
     public static int punti = 0;            //variabile per i punti
     public static int setBg;    //Variabile che mi serve per settare lo sfondo della schermata di login e menu
