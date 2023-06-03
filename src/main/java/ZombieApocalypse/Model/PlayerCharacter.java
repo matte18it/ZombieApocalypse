247ee93a1283739a4ef6c12f2b7d078464521f73
PierfrancescoN <pier.napoli@yahoo.it>
2023-06-03 18:58:59 +0200

    bug fix pt 2

--- src/main/java/ZombieApocalypse/Model/PlayerCharacter.java
+++ src/main/java/ZombieApocalypse/Model/PlayerCharacter.java
@@ -74,13 +74,13 @@ public class PlayerCharacter extends Character {
             if(speed==4)
                 speed=1;
         }
-        if(dir==movementDirection.RIGHT && (world.isGround0(getX()+Settings.CELL_SIZEX, getY())) && !world.isEnemy(getX()+20, getY()))
+        if(dir==movementDirection.RIGHT && (world.isGround0(getX()+wight+(10*speed), getY())) && !world.isEnemy(getX()+20, getY()))
             x += (10*speed);
-        else if(dir==movementDirection.LEFT && (world.isGround0(getX()-Settings.CELL_SIZEX, getY())) && !world.isEnemy(getX()-20, getY()))
+        else if(dir==movementDirection.LEFT && (world.isGround0(getX()-wight-(10*speed), getY())) && !world.isEnemy(getX()-20, getY()))
             x -= (10*speed);
-        else if(dir==movementDirection.UP && (world.isGround0(getX(), getY()-Settings.CELL_SIZEY)) && !world.isEnemy(getX(), getY()-20) )
+        else if(dir==movementDirection.UP && (world.isGround0(getX(), getY()-height-(10*speed))) && !world.isEnemy(getX(), getY()-20) )
             y -= (10*speed);
-        else if(dir==movementDirection.DOWN && (world.isGround0(getX(), getY()+Settings.CELL_SIZEY+7))&& !world.isEnemy(getX(), getY()+20))
+        else if(dir==movementDirection.DOWN && (world.isGround0(getX(), getY()+height+(10*speed)))&& !world.isEnemy(getX(), getY()+20))
             y += (10*speed);
         else
             movement=false;
