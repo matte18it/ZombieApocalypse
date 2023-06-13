0770f32ef4d1f4489182f6cd698e22e091471e7d
Matteo Canino <cnnmtt02p18c352y@studenti.unical.it>
2023-06-13 19:38:56 +0200

    Miglioramenti

--- src/main/java/ZombieApocalypse/Model/Editor/EditorBarModel.java
+++ src/main/java/ZombieApocalypse/Model/Editor/EditorBarModel.java
@@ -40,6 +40,7 @@ public class EditorBarModel {
             try { f2.createNewFile(); }
             catch (IOException e) { throw new RuntimeException(e); }
             writeOnFile(f2);
+            showConfirm();
         }
         else{
             //se il file esiste chiedo all'utente se lo vuole sovrascrivere
@@ -141,6 +142,63 @@ public class EditorBarModel {
                 f2.delete();
                 try { f2.createNewFile(); } catch (IOException ex) { throw new RuntimeException(ex); }
                 writeOnFile(f2);
+            }
+        });
+
+        dialog.getContentPane().add(pane);
+        dialog.setUndecorated(true);
+        dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        dialog.setAlwaysOnTop(true);
+        dialog.setModal(true);
+        dialog.setSize(new Dimension(515, 220));
+        dialog.setLocationRelativeTo(null);
+        dialog.setVisible(true);
+    }
+    private void showConfirm() {
+        //dialog per conferma il salvataggio
+
+        Font font = ResourcesLoader.getInstance().getFont("/Font/PixelFont.otf", 20, Font.PLAIN);
+        GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
+        UIManager.put("OptionPane.background",new Color(92,75,35));
+        UIManager.put("Panel.background",new Color(18,17,15));
+        UIManager.put("OptionPane.minimumSize",new Dimension(500,200));
+        UIManager.put("OptionPane.border", new EmptyBorder(10, 10, 10,10));
+        UIManager.put("OptionPane.font", ResourcesLoader.getInstance().getFont("/Font/PixelFont.otf", 20, Font.PLAIN));
+        UIManager.put("OptionPane.foreground", Color.WHITE);
+
+        //creo la label e gli setto il font personalizzato
+        JLabel label = new JLabel();
+        label.setFont(font.deriveFont(Font.PLAIN, 18));
+        label.setForeground(Color.WHITE);
+        label.setMinimumSize(new Dimension(100, 100));
+        label.setPreferredSize(new Dimension(100, 100));
+        label.setBorder(new EmptyBorder(10, 10, 0, 0));
+
+        JButton btnMenu = new JButton("Ok");
+        if(GameData.lang.equals(GameData.Language.IT))
+            label.setText("<html>File salvato correttamente!</html>");
+        else
+            label.setText("<html>File saved correctly!</html>");
+
+        btnMenu.setIcon(ResourcesLoader.getInstance().getImageIcon("/Login&Menu/sendButton.png", 230, 60, false));
+        btnMenu.setHorizontalTextPosition(JButton.CENTER);
+        btnMenu.setVerticalTextPosition(JButton.CENTER);
+        btnMenu.setBorderPainted(false);
+        btnMenu.setFocusPainted(false);
+        btnMenu.setContentAreaFilled(false);
+        btnMenu.setForeground(Color.WHITE);
+        btnMenu.setFont(ResourcesLoader.getInstance().getFont("/Font/PixelFont.otf", 25, Font.PLAIN));
+
+        //creo il joptionpane e gli assegno la label, poi creo il dialog e lo mostro
+        JOptionPane pane = new JOptionPane(label,  JOptionPane.PLAIN_MESSAGE,  JOptionPane.DEFAULT_OPTION,null,  new JButton[] {btnMenu});
+        JDialog dialog = new JDialog();
+        btnMenu.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                if(GameData.sound)
+                    PlayWav.getInstance().playButtonSound();
+                dialog.dispose();
             }
         });
 
